8a57aa0be8aba0b27a5403ba57dea259a8174a4d
Dias <uaudias@gmail.com>
2023-03-03 16:25:12 +0500

    upd

--- regex/main.py
+++ regex/main.py
@@ -2,18 +2,19 @@ import re
 import datetime
 
 
-password_reqs = r"^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).*$"
-email_reqs = r"^[a-z0-9]+[\._]?[ a-z0-9]+[@]\w+[. ]\w{2,3}$"
-save_date = datetime.datetime.now().strftime("%d.%m.%Y")
-save_time = datetime.datetime.now().strftime("%H_%M_%S")
+password_reqs = re.compile(r"^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).*$")
+email_reqs = re.compile(r"^[a-z0-9]+[\._]?[ a-z0-9]+[@]\w+[. ]\w{2,3}$")
+now = datetime.datetime.now()
+save_date = now.strftime("%d.%m.%Y")
+save_time = now.strftime("%H_%M_%S")
 
 while True:
     email_ = input("Enter your email (example@gmail.com): ")
-    if not re.search(email_reqs, email_):
+    if not email_reqs.search(email_):
         print("Mail must be followed requirements!")
         continue
     password = input("Your password: ")
-    if not re.search(password_reqs, password):
+    if not password_reqs.search(password):
         print("Password must be followed requirements: lengths: 8 or more, chars at least: one number, one special "
               "symbol(@#$%^&+=!), one capital letter and one lowercase letter")
         continue
