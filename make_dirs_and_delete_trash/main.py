7cbb342461f4271653d469751072ce1d0849ecd1
Dias <uaudias@gmail.com>
2023-03-03 15:30:46 +0500

    upd

--- make_dirs_and_delete_trash/main.py
+++ make_dirs_and_delete_trash/main.py
@@ -2,17 +2,23 @@ import os
 import json
 import random
 
-# for i in range(5):
+# chars = "CHeiy"
+# rand_char = random.choice(chars)
+# dir_path = f"e://Homeworker/make_dirs_and_delete_trash/temp_{rand_char}/"
+# for i in rand_char:
+#     print(i)
 #     dict1 = {f"key_{i}": f"value_{i}"}
-#     dict2 = {f"key_{i}": f"value_{i}"}
-#     dir_path = f"e://Homeworker/make_dirs_and_delete_trash/temp_{i}/"
 #     os.makedirs(dir_path, exist_ok=True)
 #     with open(os.path.join(dir_path, "my_cop.json"), "w") as file:
 #         json.dump(dict1, file)
 #     with open(os.path.join(dir_path, "not_my_cop.json"), "w") as trash_file:
-#         json.dump(dict2, trash_file)
+#         json.dump(dict1, trash_file)
 
+top = 'parent_dir'
+keep_file = 'my_cop.json'
 
-for i in range(5):
-    os.remove(f"e://Homeworker/make_dirs_and_delete_trash/temp_{i}/not_my_cop.json")
-    print("done")
+for root, dirs, files, in os.walk(top, topdown=True):
+    for name in files:
+        if name != keep_file:
+            os.remove(os.path.join(root, name))
+            print('done')
