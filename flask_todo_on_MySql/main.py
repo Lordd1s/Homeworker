7791da7bdaa28159fdeabddbde2e8309509028b8
Dias <uaudias@gmail.com>
2023-07-10 05:52:50 +0600

    upd flsk

--- flask_todo_on_MySql/main.py
+++ flask_todo_on_MySql/main.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request, redirect
-from utils import Db
+from utils import DbMySQL
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 
 
 @app.route('/')
 def index():
-    todo_rows = Db.select(query="SELECT id, title, completed FROM todos")
+    todo_rows = DbMySQL.select(query="SELECT id, title, completed FROM todos")
     todos = [{'id': row[0], 'title': row[1], 'completed': row[2]} for row in todo_rows]
     return render_template('index.html', todos=todos)
 
@@ -17,7 +17,7 @@ def add_todo():
     if len(todo) < 3:
         return render_template("404.html")
     print(todo)
-    Db.insert_to_db("INSERT INTO todos (title) VALUES (?)", (todo,))
+    DbMySQL.insert_to_db("INSERT INTO todos (title) VALUES (%s)", (todo,))
     return redirect('/')
 
 
@@ -25,21 +25,21 @@ def add_todo():
 def complete_todo():
     todo_id = request.form.get('id')
 
-    completed = Db.select_one("SELECT completed FROM todos WHERE id = ?", one_val=(todo_id,))
+    completed = DbMySQL.select_one("SELECT completed FROM todos WHERE id = %s", one_val=(todo_id,))
     print(completed)
 
     if completed[0] == 1:
-        Db.update(query="UPDATE todos SET completed = 0 WHERE id = ?", upd_data=(todo_id,))
+        DbMySQL.update(query="UPDATE todos SET completed = 0 WHERE id = %s", upd_data=(todo_id,))
         return redirect('/')
 
-    Db.update(query="UPDATE todos SET completed = 1 WHERE id = ?", upd_data=(todo_id,))
+    DbMySQL.update(query="UPDATE todos SET completed = 1 WHERE id = %s", upd_data=(todo_id,))
 
     return redirect('/')
 
 
 @app.route('/delete/<int:todo_id>', methods=['POST'])
 def delete_todo(todo_id):
-    Db.delete_from_db("DELETE FROM todos WHERE id = ?", todo_id)
+    DbMySQL.delete_from_db("DELETE FROM todos WHERE id = %s", todo_id)
     return redirect('/')
 
 
@@ -47,7 +47,7 @@ def delete_todo(todo_id):
 def update_todo(todo_id):
     new_title = request.form.get('todo')
 
-    Db.update("UPDATE todos SET title = ? WHERE id = ?", (new_title, todo_id))
+    DbMySQL.update("UPDATE todos SET title = %s WHERE id = %s", (new_title, todo_id))
 
     return redirect('/')
 
